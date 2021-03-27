b46c2d53f4c4a5fbf725d6dfcbb9641fccc132db
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2021-03-28 00:03:22 +0530

    layout change

--- js/index.js
+++ js/index.js
@@ -1,46 +1,60 @@
 
-$(window).scroll(startCounter);
-// $(window).scroll(secCounter);
+// $(window).scroll(startCounter);
+// // $(window).scroll(secCounter);
 
-function startCounter() {
-    if (elementInViewport(document.querySelector('.teams-cont'))) {
-        $(window).off("scroll", startCounter);
-			var group93 = document.querySelector('#Group-93');
-      group93.style.opacity = '1';
-			var group94 = document.querySelector('#Group-94');
-      group94.style.opacity = '1';
-			var group95 = document.querySelector('#Group-95');
-      group95.style.opacity = '1';
-			var group96 = document.querySelector('#Group-96');
-      group96.style.opacity = '1';
-			var group97 = document.querySelector('#Group-97');
-      group97.style.opacity = '1';
-			var group98 = document.querySelector('#Group-98');
-      group98.style.opacity = '1';
-			var group99 = document.querySelector('#Group-99');
-      group99.style.opacity = '1';
-			var group100 = document.querySelector('#Group-100');
-      group100.style.opacity = '1';
-			var group101 = document.querySelector('#Group-101');
-      group101.style.opacity = '1';
-			var group102 = document.querySelector('#Group-102');
-      group102.style.opacity = '1';
-			var group103 = document.querySelector('#Group-103');
-      group103.style.opacity = '1';
-			var group92 = document.querySelector('#Group-92');
-      group92.style.opacity = '1';
-			var group87 = document.querySelector('#Group-87');
-      group87.style.opacity = '1';
-			var group88 = document.querySelector('#Group-88');
-      group88.style.opacity = '1';
-			var group89 = document.querySelector('#Group-89');
-      group89.style.opacity = '1';
-			var group90 = document.querySelector('#Group-90');
-      group90.style.opacity = '1';
-			var group91 = document.querySelector('#Group-91');
-      group91.style.opacity = '1';
-	}
-}
+// function startCounter() {
+//     if (elementInViewport(document.querySelector('.teams-cont'))) {
+//         $(window).off("scroll", startCounter);
+// 			var group93 = document.querySelector('#Group-93');
+//       group93.style.opacity = '1';
+// 			var group94 = document.querySelector('#Group-94');
+//       group94.style.opacity = '1';
+// 			var group95 = document.querySelector('#Group-95');
+//       group95.style.opacity = '1';
+// 			var group96 = document.querySelector('#Group-96');
+//       group96.style.opacity = '1';
+// 			var group97 = document.querySelector('#Group-97');
+//       group97.style.opacity = '1';
+// 			var group98 = document.querySelector('#Group-98');
+//       group98.style.opacity = '1';
+// 			var group99 = document.querySelector('#Group-99');
+//       group99.style.opacity = '1';
+// 			var group100 = document.querySelector('#Group-100');
+//       group100.style.opacity = '1';
+// 			var group101 = document.querySelector('#Group-101');
+//       group101.style.opacity = '1';
+// 			var group102 = document.querySelector('#Group-102');
+//       group102.style.opacity = '1';
+// 			var group103 = document.querySelector('#Group-103');
+//       group103.style.opacity = '1';
+// 			var group92 = document.querySelector('#Group-92');
+//       group92.style.opacity = '1';
+// 			var group87 = document.querySelector('#Group-87');
+//       group87.style.opacity = '1';
+// 			var group88 = document.querySelector('#Group-88');
+//       group88.style.opacity = '1';
+// 			var group89 = document.querySelector('#Group-89');
+//       group89.style.opacity = '1';
+// 			var group90 = document.querySelector('#Group-90');
+//       group90.style.opacity = '1';
+// 			var group91 = document.querySelector('#Group-91');
+//       group91.style.opacity = '1';
+// 	}
+// }
+
+  jqSimpleConnect.connect(".circle-connect-1",".svg", {
+  color:'green',
+  radius: 3,
+  anchorA:'horizontal',
+  anchorB:'vertical'
+});
+
+  jqSimpleConnect.connect(".circle-connect-2",".svg", {
+  color:'green',
+  radius: 3,
+  anchorA:'horizontal',
+  anchorB:'vertical'
+});
 
 if (window.innerWidth >= 1200) {
   jqSimpleConnect.connect(".elemA",".elemB", {
@@ -96,20 +110,6 @@ jqSimpleConnect.connect(".elemI",".elemE", {
 jqSimpleConnect.connect(".elemJ",".elemK", {
   color:'green',
   radius: 3,
-});
-
-  jqSimpleConnect.connect(".circle-connect-1","#Group-93", {
-  color:'green',
-  radius: 3,
-  anchorA:'horizontal',
-  anchorB:'vertical'
-});
-
-  jqSimpleConnect.connect(".circle-connect-2","#Group-102", {
-  color:'green',
-  radius: 3,
-  anchorB:'horizontal',
-  anchorA:'vertical'
 });
 }
 
